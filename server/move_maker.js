7b3a0939c643c5863b716a930efb2198816d2d7a
Adam Brown <adam@novoda.com>
2016-10-11 20:10:36 +0100

    add swift support and empty example

--- server/move_maker.js
+++ server/move_maker.js
@@ -1,18 +1,23 @@
 var nodeLoader = require('./codeloaders/node_loader').load;
 var javaLoader = require('./codeloaders/java_loader').load;
+var swiftLoader = require('./codeloaders/swift_loader').load;
 
 function getMove(loadedPlayer, board) {
    return getLoader(loadedPlayer)(`../players/${loadedPlayer.path}`, board);
 }
 
 function getLoader(player) {
-  if (player.language === 'javascript') {
-    return nodeLoader;
+
+  switch (player.language) {
+    case 'javascript':
+      return nodeLoader;
+    case 'java':
+      return javaLoader;
+    case 'swift':
+      return swiftLoader;
+    default:
+      throw err;
   }
-  if (player.language.endsWith("java")) {
-    return javaLoader;
-  }
-  throw err;
 }
 
 module.exports.getMove = getMove;
